e3d4499e4ec6ec454e0c84cbebb6e71d72ff6a16
A. Lebedeva <Alina.Lebedeva29@yandex.ru>
2021-06-29 13:19:38 +0300

    Add all

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Language model GPT-2 for generate text in style of V. V. Maykovsky',
+    author='Alina Lebedeva',
+    license='',
+)
